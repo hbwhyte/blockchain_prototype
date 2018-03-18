3bb31d525050b1b4134fdf083ee2a894334626e5
hbwhyte <heather.b.whyte@gmail.com>
2018-03-18 20:03:52 +0800

    Turn hashes into blockchain and check validity

--- src/blockchain_package/MyFirstChain.java
+++ src/blockchain_package/MyFirstChain.java
@@ -1,17 +1,39 @@
 package blockchain_package;
 
+import java.util.ArrayList;
+import com.google.gson.GsonBuilder;
+
 public class MyFirstChain {
+
+    public static ArrayList<Block> blockchain = new ArrayList<>();
 
     public static void main(String[] args) {
 
-        Block genesisBlock = new Block("Hello World!", "0");
-        System.out.println("Hash for block 1: " + genesisBlock.hash);
+        blockchain.add(new Block("Hello World!", "0"));
+        blockchain.add(new Block("Ooh the first two-sided block!", blockchain.get(blockchain.size()-1).hash));
+        blockchain.add(new Block("I\'m number three, rounding it out", blockchain.get(blockchain.size()-1).hash));
 
-        Block secondBlock = new Block("Ooh the first full block!", genesisBlock.hash);
-        System.out.println("Hash for block 2: " + secondBlock.hash);
+        String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println(blockchainJson);
+    }
 
-        Block thirdBlock = new Block("I'm number three, rounding it out", secondBlock.hash);
-        System.out.println("Hash for block 3: " + thirdBlock.hash);
+    public static boolean isChainValid() {
+        Block currentBlock;
+        Block previousBlock;
 
+        // Loop through the blocks to check hashes
+        for (int i = 0; i < blockchain.size(); i++) {
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i-1);
+
+            if (!currentBlock.hash.equals(currentBlock.calculateHash())){
+                return false;
+            }
+            if (!previousBlock.hash.equals(previousBlock.calculateHash())){
+                return false;
+            }
+
+        }
+        return true;
     }
 }
