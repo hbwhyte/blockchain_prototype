a29e3df1411451c612fa2fbed5f34b87314dec45
hbwhyte <heather.b.whyte@gmail.com>
2018-04-14 15:50:29 +0800

    Add transactions to block

--- blockchain/src/main/java/bali_coin/blockchain/Block.java
+++ blockchain/src/main/java/bali_coin/blockchain/Block.java
@@ -1,41 +1,62 @@
 package bali_coin.blockchain;
 
+import bali_coin.transactions.Transaction;
 import bali_coin.utilities.StringUtil;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Block {
 
     public String hash;
     public String previousHash;
-    private String data; // Our simple message to be transferred
+    public String merkleRoot;
+    public ArrayList<Transaction> transactions = new ArrayList<Transaction>(); //our data will be a simple message.
     private long timeStamp; // As milliseconds since 1/1/1970
     private int nonce;
 
-    public Block(String data, String previousHash) {
-        this.data = data;
+    public Block(String previousHash) {
         this.previousHash = previousHash;
         this.timeStamp = new Date().getTime();
         this.hash = calculateHash(); // Last item, only after the previous variables are set
     }
 
+    //Calculate new hash based on blocks contents
     public String calculateHash() {
-        String calculatedHash = StringUtil.applySha256(
+        String calculatedhash = StringUtil.applySha256(
                 previousHash +
                         Long.toString(timeStamp) +
                         Integer.toString(nonce) +
-                        data
+                        merkleRoot
         );
-        return calculatedHash;
+        return calculatedhash;
     }
 
+    //Increases nonce value until hash target is reached.
     public void mineBlock(int difficulty) {
-        String target = new String(new char[difficulty]).replace('\0','0');
-        while(!hash.substring(0, difficulty).equals(target)) {
-            nonce++;
+        merkleRoot = StringUtil.getMerkleRoot(transactions);
+        //TODO add getDifficultyString Method
+        String target = StringUtil.getDificultyString(difficulty); //Create a string with difficulty * "0"
+        while(!hash.substring( 0, difficulty).equals(target)) {
+            nonce ++;
             hash = calculateHash();
         }
-        System.out.println("Block Mined! : " + hash);
+        System.out.println("Block Mined!!! : " + hash);
+    }
+
+    //Add transactions to this block
+    public boolean addTransaction(Transaction transaction) {
+        //process transaction and check if valid, unless block is genesis block then ignore.
+        if(transaction == null) return false;
+        if((previousHash != "0")) {
+            if((transaction.processTransaction() != true)) {
+                System.out.println("Transaction failed to process. Discarded.");
+                return false;
+            }
+        }
+        transactions.add(transaction);
+        System.out.println("Transaction Successfully added to Block");
+        return true;
     }
 
 }
